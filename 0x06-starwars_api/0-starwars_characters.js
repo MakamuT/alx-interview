8aa729a9052580ccd2e3ec41cc8babbb7ff7f4cc
Thembisile  Makamu <makamuthe2@gmail.com>
2024-09-20 01:08:23 +0200

    starwars

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,33 +1,32 @@
-const axios = require("axios");
+const fetch = require("node-fetch");
 
 async function getMovieCharacters(movieId) {
   const movieUrl = `https://swapi.dev/api/films/${movieId}/`;
 
   try {
-    const movieResponse = await axios.get(movieUrl);
-    const movieData = movieResponse.data;
+    const movieResponse = await fetch(movieUrl);
+    if (!movieResponse.ok) {
+      throw new Error(`Error: Received status code ${movieResponse.status}`);
+    }
 
+    const movieData = await movieResponse.json();
     console.log(`Characters in '${movieData.title}':\n`);
 
-    // Fetch all character data concurrently
-    const characterPromises = movieData.characters.map((characterUrl) =>
-      axios.get(characterUrl)
-    );
-    const characterResponses = await Promise.all(characterPromises);
-
-    // Print all character names
-    characterResponses.forEach((characterResponse) => {
-      const characterData = characterResponse.data;
+    // Fetch character names
+    for (let characterUrl of movieData.characters) {
+      const characterResponse = await fetch(characterUrl);
+      const characterData = await characterResponse.json();
       console.log(characterData.name);
-    });
+    }
   } catch (error) {
     console.error(`Error fetching data: ${error.message}`);
   }
 }
 
+// Get the movie ID from the command line argument
 const movieId = process.argv[2];
 if (!movieId) {
-  console.error("Usage: node script.js <Movie ID>");
+  console.error("Usage: node 0-starwars_characters.js <Movie ID>");
 } else {
   getMovieCharacters(movieId);
 }
